5280f929a1dc4ea0215f443e861ef1320054a6cf
fabianohipolito <fabiano-hipolito@hotmail.com>
2021-09-10 08:30:23 -0300

    Ajustando arquivos para criação da vm openstack e iac

--- openstack/openstack.sh
+++ openstack/openstack.sh
@@ -4,9 +4,9 @@ sudo useradd -s /bin/bash -d /opt/stack -m stack
 
 echo "stack ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/stack
 
-sudo su - stack
-git clone https://opendev.org/openstack/devstack 
 mv vagrant local.conf
-mv local.conf devstack/
-mv devstack/ /home/stack
-#bash /home/stack/devstack/stack.sh
+#mv vagrant python
+runuser -l stack -c "git clone https://opendev.org/openstack/devstack" 
+mv /home/vagrant/local.conf /opt/stack/devstack
+#runuser -l stack -c "bash /opt/stack/devstack/stack.sh"
+
