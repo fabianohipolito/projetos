6def5ccf54ca82df06d2e40cb9cb4964de5c9221
fabianohipolito <fabiano-hipolito@hotmail.com>
2021-12-07 20:04:04 -0300

    Adicionando novas maquinas para configuração do monitoramento com prometheus zabbix e gafrana e graylog

--- provision/centos.sh
+++ provision/centos.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
  yum install epel-release -y
- yum install ansible git wget  -y
+ yum install ansible git wget vim  -y
  
  yum install -y yum-utils
  yum-config-manager --add-repo  https://download.docker.com/linux/centos/docker-ce.repo
